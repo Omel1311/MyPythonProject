b3ebd51d06902bec0bbc0a2e01f90b31a2cf5521
Omel1311 <omel1311@gmail.com>
2023-04-09 19:25:07 +0300

    connection mysql/python2

--- SQL/main.py
+++ SQL/main.py
@@ -9,8 +9,55 @@ try:
         user=user,
         password=password,
         database=db_name,
-        cursorclass=pymysql.cursors.DictCursor)
+        #cursorclass=pymysql.cursors.DictCursor)
+        cursorclass = pymysql.cursors.Cursor)
     print('successfully connected...')
+    print('#'*30)
+
+    try:
+        '''
+         # создать таблицу в  mysql
+        with connection.cursor() as cursor:
+            create_table = "create table test22(id int primary key, name varchar(20));"
+            cursor.execute(create_table)
+            print('create_table succsesfully')
+
+        '''
+        '''
+        # вставить данные в таблицу в  mysql
+        with connection.cursor() as cursor:
+            insert_table = "insert into test33 values(5, 'lanna');"
+            cursor.execute(insert_table)
+            connection.commit()
+            print('insert Table succsesfully')
+        
+        '''
+
+        '''
+        # создать базу данных в mysql
+        with connection.cursor() as cursor:
+            create_db = "create database python_sql"
+            cursor.execute(create_db)
+            connection.commit()
+            print('create db succsesfully')
+        '''
+        '''
+        # посмотреть даитабейсы в mysql
+        with connection.cursor() as cursor:
+            cursor.execute('show databases')
+            for db in cursor:
+                print(db)
+        '''
+         # посмотреть таблицы в базе данных
+        with connection.cursor() as cursor:
+             cursor.execute('show tables')
+             for t in cursor:
+                 print(t)
+
+    finally:
+        connection.close()
+
+
 except Exception as ex:
     print('Connection refused...')
     print(ex)
